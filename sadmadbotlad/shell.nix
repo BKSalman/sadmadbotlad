a83b7e6beb85bb20f7f5b856a94b7b006e9a70d8
BKSalman <kfoo2023@gmail.com>
2023-03-24 22:15:53 +0300

    edit shell file

--- sadmadbotlad/shell.nix
+++ sadmadbotlad/shell.nix
@@ -1,29 +1,26 @@
-let
-  system = "x64_86-linux";
-  unstable = import (fetchTarball https://github.com/nixos/nixpkgs/archive/nixpkgs-unstable.tar.gz) { };
-  # masterpkgs = import builtins.fetchTarball https://github.com/nixos/nixpkgs/master.tar.gz {};
-in
-{ pkgs ? import <nixpkgs> { allowUnfree = true; }}: 
-   pkgs.mkShell rec {
-    buildInputs = with pkgs; [
-      # unstable.mpv-unwrapped.dev
-      # clang
-      # llvmPackages.libclang
-      # llvmPackages.libcxxClang
-      # llvmPackages.bintools
-      yt-dlp
-      alsa-lib
-      rocksdb
-      unstable.mpv
-      gcc
+{ pkgs ? import <nixpkgs> { }}: 
+   pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+      # mold
+      clang
+      llvmPackages.libclang
+      llvmPackages.libcxxClang
+      llvmPackages.bintools
+      mpv
+      # gcc
       openssl
       libiconv
       pkg-config
+      rocksdb
+      alsa-lib
     ];
-    LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-    BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.lib.getVersion pkgs.clang}/include";
+    
+    buildInputs = with pkgs; [
+      yt-dlp
+    ];
+    # LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+    
+    # BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.lib.getVersion pkgs.clang}/include";
     ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib/";
     ROCKSDB_STATIC = "true";
   }
-
-
