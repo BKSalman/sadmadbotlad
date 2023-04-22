69125a64fc240a6f950efa7c2ffec8279b5fdce7
BKSalman <salman.f.abuhaimed@gmail.com>
2023-04-22 10:10:29 +0300

    flake update

---
+++ sadmadbotlad/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "sadmadbotlad flake";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system: let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        {
+          devShell = pkgs.mkShell rec {
+            packages = with pkgs; [
+              playerctl
+            ];
+            
+            nativebuildInputs = with pkgs; [
+                dbus
+                alsa-lib
+            ];
+
+            buildInputs = with pkgs; [
+                mpv
+                clang
+                llvmPackages.libclang
+                llvmPackages.libcxxClang
+                llvmPackages.bintools
+                openssl
+                libiconv
+                pkg-config
+                rocksdb
+            ];
+              LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+              BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.lib.getVersion pkgs.clang}/include";
+              ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib/";
+              ROCKSDB_STATIC = "true";
+              LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+          };
+      });
+}
